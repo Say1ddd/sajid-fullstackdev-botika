3f2fd7461eece9d664b8aa4c934f4c0e6b5d471a
Say1ddd <sajidmuhammadikhlas@gmail.com>
2024-05-28 18:39:22 +0700

    Added: factory for karyawan, pekerjaan, divisi

---
+++ database/factories/DivisiFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Divisi>
+ */
+class DivisiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama_divisi' => fake()->word(),
+            'deskripsi_divisi' => fake()->text(),
+            'lokasi' => fake()->city(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Divisi;
+use App\Models\Karyawan;
+use App\Models\Pekerjaan;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,6 +17,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Divisi::factory(10)->create();
+        Pekerjaan::factory(10)->create();
+        Karyawan::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
